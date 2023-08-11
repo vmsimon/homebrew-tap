9c0f52653f261100b5f8ba8debc57d119a276057
Volker Meyer-Simon <vmsimon@oev.de>
2023-08-11 08:10:39 +0200

    Create cloudctl.rb

---
+++ formula/cloudctl.rb
@@ -0,0 +1,44 @@
+class Cloudctl < Formula
+  release = "0.11.13"
+  desc "Commandline client for Kubernetes as a Service and more!"
+  homepage "https://github.com/fi-ts/cloudctl"
+  url "https://github.com/fi-ts/cloudctl/"
+  license ""
+  version release
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-darwin-arm64"
+      sha256 "80d7e3b8d6b50fc10dbf6e853fc388f060655e6c3e60929decad632f57d5431e"
+    else
+      url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-darwin-amd64"
+      sha256 "29eb9f445bceadf338a51eedcb14bb8ee1e7d54e32d6c91a240b5b252618b60a"
+    end
+  elsif OS.linux?
+      url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-linux-amd64"
+      sha256 "0b969610a349cc2ab8e0ccc0a97118492ff834bf63f839811ac72468e1835abb"
+  end
+
+  def install
+    if OS.mac?
+      if Hardware::CPU.arm?
+        bin.install "cloudctl-darwin-arm64" => "cloudctl"
+      else
+        bin.install "cloudctl-darwin-amd64" => "cloudctl"
+      end
+    elsif OS.linux?
+      bin.install "cloudctl-linux-amd64" => "cloudctl"
+    end
+  end
+
+  def caveats
+    <<~EOS
+    
+        To activate the completions, add the following to your .zshrc:
+
+          [[ -e $(brew --prefix)/bin/cloudctl ]] && eval "$(cloudctl completion zsh)"
+        
+        There are also completions for bash and fish available.
+        
+    EOS
+  end
+end
