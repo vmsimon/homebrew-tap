6ad5b23072ed3a5679a6920a3fc78c3590152215
vmsimon <vmsimon@users.noreply.github.com>
2023-08-11 12:18:05 +0000

    New fi-ts/cloudctl release version

--- formula/cloudctl.rb
+++ formula/cloudctl.rb
@@ -1,5 +1,5 @@
 class Cloudctl < Formula
-  release = "0.11.12"
+  release = "0.11.13"
   desc "Commandline client for Kubernetes as a Service and more!"
   homepage "https://github.com/fi-ts/cloudctl"
   url "https://github.com/fi-ts/cloudctl/"
@@ -8,7 +8,7 @@ class Cloudctl < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-darwin-arm64"
-      sha256 "#sha256-darwin-arm64"
+      sha256 "80d7e3b8d6b50fc10dbf6e853fc388f060655e6c3e60929decad632f57d5431e"
       def install
         bin.install "cloudctl-darwin-arm64" => "cloudctl"
       end
@@ -16,14 +16,14 @@ class Cloudctl < Formula
   end
     else
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-darwin-amd64"
-      sha256 "#sha256-darwin-amd64" 
+      sha256 "29eb9f445bceadf338a51eedcb14bb8ee1e7d54e32d6c91a240b5b252618b60a" 
       def install
         bin.install "cloudctl-darwin-amd64" => "cloudctl"
       end
     end
   elsif OS.linux?
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-linux-amd64"
-      sha256 "#sha256-linux-amd64"
+      sha256 "0b969610a349cc2ab8e0ccc0a97118492ff834bf63f839811ac72468e1835abb"
       def install
         bin.install "cloudctl-linux-arm64" => "cloudctl"
       end
