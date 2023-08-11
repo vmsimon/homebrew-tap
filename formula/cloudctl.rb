911f9fe728a37778954360f93ee0a5a9a8f97d18
Volker Meyer-Simon <vmsimon@oev.de>
2023-08-11 15:05:19 +0200

    Update cloudctl.rb

--- formula/cloudctl.rb
+++ formula/cloudctl.rb
@@ -9,15 +9,9 @@ class Cloudctl < Formula
     if Hardware::CPU.arm?
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-darwin-arm64"
       sha256 "80d7e3b8d6b50fc10dbf6e853fc388f060655e6c3e60929decad632f57d5431e"
-      def install
-        bin.install "cloudctl-darwin-arm64" => "cloudctl"
-      end
     else
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-darwin-amd64"
       sha256 "29eb9f445bceadf338a51eedcb14bb8ee1e7d54e32d6c91a240b5b252618b60a" 
-      def install
-        bin.install "cloudctl-darwin-amd64" => "cloudctl"
-      end
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
@@ -25,10 +19,11 @@ class Cloudctl < Formula
     else
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-linux-amd64"
       sha256 "0b969610a349cc2ab8e0ccc0a97118492ff834bf63f839811ac72468e1835abb"
-      def install
-        bin.install "cloudctl-linux-amd64" => "cloudctl"
-      end
     end
+  end
+
+  def install
+    bin.install Dir.glob("cloudctl-*").first => "cloudctl"
   end
 
   def caveats
