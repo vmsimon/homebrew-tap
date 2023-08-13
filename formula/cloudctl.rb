85bb988f5d35437e6ceee9471830b63afabda848
vmsimon <vmsimon@users.noreply.github.com>
2023-08-13 01:13:29 +0000

    New fi-ts/cloudctl release version

--- formula/cloudctl.rb
+++ formula/cloudctl.rb
@@ -1,5 +1,5 @@
 class Cloudctl < Formula
-  release = "null"
+  release = "0.11.13"
   desc "Commandline client for Kubernetes as a Service and more!"
   homepage "https://github.com/fi-ts/cloudctl"
   url "https://github.com/fi-ts/cloudctl/"
