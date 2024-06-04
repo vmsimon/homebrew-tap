0576055933da501d6e9a36a1a34a9135cbe47865
vmsimon <vmsimon@users.noreply.github.com>
2024-06-04 01:20:46 +0000

    New fi-ts/cloudctl release version

--- formula/cloudctl.rb
+++ formula/cloudctl.rb
@@ -1,5 +1,5 @@
 class Cloudctl < Formula
-  release = "0.12.19"
+  release = "0.12.20"
   desc "Commandline client for Kubernetes as a Service and more!"
   homepage "https://github.com/fi-ts/cloudctl"
   url "https://github.com/fi-ts/cloudctl/"
@@ -8,17 +8,17 @@ class Cloudctl < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-darwin-arm64"
-      sha256 "0e36523ff8ace1d24c1c2561a9d244a7b17ff74e86ab9f2bd07d5549955edd60"
+      sha256 "5a763f79a784f56c971ac67498fee4450c8c47af3efce09235ae452905fe5244"
     else
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-darwin-amd64"
-      sha256 "6337c702fbd5ff1c07f222e2c3800f076ba09a71ed271d662a1a1f8e7ef1e2cc" 
+      sha256 "a7a713779160941349a048a13d44b81b5c1dcafb2406a6b4e0104349bf324b18" 
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       #atm not supported
     else
       url "https://github.com/fi-ts/cloudctl/releases/download/v#{version}/cloudctl-linux-amd64"
-      sha256 "02f83c7de0a25b74c427d5e5e974758ed1a9c43cb9f460599bbfaf6ee42904c8"
+      sha256 "6826920745c350f6e296a00ecd9b85ec6852d7c69ba1348798386385b2aa4176"
     end
   end
 
